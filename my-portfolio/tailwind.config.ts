bd7a853c33187386aa454de26be09ad5b3919058
Sujit-Thapa <sujitthapa.professiona@gmaill.com>
2025-02-03 22:58:53 +0545

    dep

--- my-portfolio/tailwind.config.ts
+++ my-portfolio/tailwind.config.ts
@@ -1,9 +1,8 @@
 import type { Config } from "tailwindcss"
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  prefix: "",
   theme: {
     container: {
       center: true,
@@ -13,9 +12,6 @@ const config = {
       },
     },
     extend: {
-      fontFamily: {
-        mono: ["var(--font-space-mono)"],
-      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -56,10 +52,27 @@ const config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        mono: ["var(--font-space-mono)"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-} satisfies Config
+  plugins: [require("tailwindcss-animate")],
+}
 
 export default config
 
